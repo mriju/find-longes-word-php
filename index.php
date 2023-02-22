5038d8016e4dfc3c88c531a6d95f427b931c83f9
Manjurul Hoque <55277013+mriju@users.noreply.github.com>
2023-02-22 09:19:28 +0600

    Update index.php
    with description

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
 function longestword($string){
-    $arraudata = explode(" ",$string);
-    $result = "";
-    foreach($arraudata as $value)
+    $arraudata = explode(" ",$string); //explode() function breaks a string into an array
+    $result = ""; // taken an empty variable
+    foreach($arraudata as $value) //every loop iteration, the value of the current array element is assigned to $value and the array pointer is moved by one, until it reaches the last array element.
     {
-        if(strlen($value) > strlen($result))
+        if(strlen($value) > strlen($result)) //strlen() function returns the length of a string
         {
-        $result = $value; 
-        
-        }
-       
+            $result = $value; //with iteration every time it find the largest value when find bellow the larger one that time if condition return false
+        }       
     }
-    return $result;
+    return $result; // return largest value
 }
 echo longestword('The quick brown fox jumped over the lazy dog');
